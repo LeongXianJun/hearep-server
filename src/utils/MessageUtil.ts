f5371f177c3ba145978ca3b846f50d5687ddf3ff
LeongXianJun <leongxianjun@1utar.my>
2020-06-28 21:06:37 +0800

    Util Update for Mobile

--- src/utils/MessageUtil.ts
+++ src/utils/MessageUtil.ts
@@ -1,7 +1,7 @@
 import { messaging } from '../connections'
 
 const sendMessages = (data: { token: string, [ key: string ]: string }[]) =>
-  messaging.sendAll(data.map(({ token, ...other }) => ({ token, data: other })))
+  messaging.sendAll(data.map(({ token, ...other }) => ({ token, data: other, notification: { title: other.title, body: other.description }, android: { priority: 'high' }, apns: { payload: { aps: { contentAvailable: true } } } })))
     .then((response) => {
       console.log('Successfully sent message:', response.successCount, response.failureCount)
       if (response.failureCount > 0)
@@ -12,7 +12,7 @@ const sendMessages = (data: { token: string, [ key: string ]: string }[]) =>
     })
 
 const sendMultipleDevices = (deviceToken: string[], data: { [ key: string ]: string }) =>
-  messaging.sendMulticast({ data: data, tokens: deviceToken })
+  messaging.sendMulticast({ data: data, notification: { title: data.title, body: data.description }, tokens: deviceToken, android: { priority: 'high' }, apns: { payload: { aps: { contentAvailable: true } } } })
     .then((response) => {
       console.log('Successfully sent message:', response.successCount, response.failureCount)
       if (response.failureCount > 0)
